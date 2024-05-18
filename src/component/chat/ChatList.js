5906b6d60671906e6aa663fe6584e3856496c4e0
Yejin427 <yejinlala427@naver.com>
2024-05-19 01:50:26 +0900

    add main page

--- src/component/chat/ChatList.js
+++ src/component/chat/ChatList.js
@@ -2,24 +2,75 @@ import React from "react";
 import styled from "styled-components";
 
 const ChatHeader = styled.div`
-  
+  padding: 1em;
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 1px grey solid;
+  .title{
+    font-size: x-large;
+  }
 `;
-const ChatContainer = styled.div`
 
+const ChatContainer = styled.div`
+  overflow: scroll;
+  padding: 1rem;
+  flex: 1;
+  position: relative;
 `;
 
 const ChatItem = styled.div`
-
+  display: flex;
+  .sender{
+    font: bold;
+  }
+  .time{
+    marginLeft: 4px;
+    font-size: small;
+    color: grey;
+  }
+  .msg{
+    margin-top: 4px;
+  }
+  &+&{
+    margin-top: 1rem;
+  }
 `;
 
-const MessageItem = styled.div`
-
+const MessageInput = styled.input`
+  position: absolute;
+  bottom: 0;
+  width: 50%;
+  height: 40px;
+  border-radius: 1em;
+  border: none;
+  background: #ebedef;
 `;
 
 const ChatList = ({channelName, chatArr}) => {
   return (
-    <div>
-      
+    <div style={{display: "flex", flex: "1", flexDirection: "column", height: "100vh"}}>
+      <ChatHeader>
+        <div className="title">
+          <img src="sharp.svg" alt="sharp"/>
+          {channelName}
+        </div>
+        <img src="member.png" alt="member"/>
+      </ChatHeader>
+      <ChatContainer>
+        {chatArr.map((chat, index) =>(
+          <ChatItem>
+            <img src="discord-circle.png" alt="discord"/>
+            <div style={{marginLeft: "4px"}}>
+              <div style={{display: "flex"}}>
+                <div className="sender">{chat.sender}</div>
+                <div className="time">{chat.time}</div>
+              </div>
+              <div className="msg">{chat.msg}</div>
+            </div>
+          </ChatItem>
+        ))}
+        <MessageInput placeholder={"Message #" + channelName}/>
+      </ChatContainer>
     </div>
   );
 }
--- src/pages/main/MainPage.js
+++ src/pages/main/MainPage.js
@@ -9,14 +9,20 @@ const MainPage = ({name}) => {
   const serverArr = ["apple", "banana"];
   const selectedServerIdx = 0;
   const chatRoomArr = ["Room1", "Room2", "Room3"];
-  
+  const chatArr = [
+    { sender: 'Alice', time: '10:00 AM', msg: 'Hello!' },
+    { sender: 'Bob', time: '10:05 AM', msg: 'Hi Alice!' },
+    { sender: 'Alice', time: '10:06 AM', msg: 'How are you?' },
+    { sender: 'Bob', time: '10:07 AM', msg: 'I am good, thank you!' }
+  ];
+
   return(
     <div style={{display: "flex", height: "100vh", width: "100vw"}}>
       {/* <ServerList />
       <NoChat name={name}/> */}
       <ServerList serverArr={serverArr} selectedIdx={selectedServerIdx}/>
       <ChatRoom name={name} serverName="My Server" chatRoomArr={chatRoomArr} selectedIdx={2}/>
-      <ChatList />
+      <ChatList channelName={chatRoomArr[2]} chatArr={chatArr}/>
     </div>
   );
 }
